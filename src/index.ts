6600911b7da6601a5741f0d1720b1b9a1f1735ee
pujiyanto <pujiyanto2650@gmail.com>
2022-11-07 22:32:43 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+	return res.status(200).send({
+		response: "Express TypeScript"
+	});
+})
+
+app.listen(process.env.APP_PORT, () => {
+	console.log((`${process.env.APP_NAME} running on port ${process.env.APP_PORT}`))
+});
